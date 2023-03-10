d8ea863994c9e6ab1d4df514259beff58cde5c56
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-03-09 23:53:19 -0300

    Criando teste utilizando do when(quando determinado método for chamado eu garanto o resultado dele(thenReturn), verify(verifico se o método foi chamado- podendo passar algum valor e vezes para verificar) e a assertions(assertivas para validar se o valor esperado é o que atribuimos). mockito/junit.

---
+++ src/test/java/FinalizarLeilaoServiceTest.java
@@ -0,0 +1,72 @@
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.service.FinalizarLeilaoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class FinalizarLeilaoServiceTest {
+
+    private FinalizarLeilaoService service;
+
+    @Mock
+    private LeilaoDao leilaoDao;
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.initMocks(this);
+        this.service = new FinalizarLeilaoService(leilaoDao);
+    }
+
+    @Test
+    void deveriaFinalizarUmLeiao() {
+        //inicializo com os valores que vou precisar
+        List<Leilao> leiloes = leiloes();
+
+        //quando a essa ação foi feita
+        when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+        service.finalizarLeiloesExpirados();
+
+        Leilao leilao = leiloes.get(0);
+        // resultado esperado
+        Assertions.assertTrue(leilao.isFechado());
+        Assertions.assertEquals(new BigDecimal(900), leilao.getLanceVencedor().getValor());
+
+        //verificar se o dao feito, no caso o que ele salvou foi esse primeiro leilao
+        verify(leilaoDao).salvar(leilao);
+    }
+
+
+    private List<Leilao> leiloes() {
+        List<Leilao> lista = new ArrayList<>();
+
+        Leilao leilao = new Leilao("Celular",
+                new BigDecimal("500"),
+                new Usuario("Fulano"));
+
+        Lance primeiro = new Lance(new Usuario("Beltrano"),
+                new BigDecimal("600"));
+        Lance segundo = new Lance(new Usuario("Ciclano"),
+                new BigDecimal("900"));
+
+        leilao.propoe(primeiro);
+        leilao.propoe(segundo);
+
+        lista.add(leilao);
+
+        return lista;
+
+    }
+}
